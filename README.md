a4253d5a8a39f510bf98af4deda2526bdb15ac1b
Jose Luiz dos Santos Xavier <jlxavier@rd.com.br>
2021-10-27 07:28:25 -0300

    Webpack Round 6

--- README.md
+++ README.md
@@ -4,4 +4,9 @@
 npm install webpack@5.28.0 --save-dev 
 npm install webpack-cli@4.5.0 --save-dev
 npm install html-webpack-plugin@5.3.1 --save-dev
+npm install copy-webpack-plugin@8.1.0 --save-dev
+npm install bootstrap@4.6.0 --save
+npm install css-loader@5.2.0 --save-dev
+npm install style-loader@2.0.0 --save-dev
+npm install mini-css-extract-plugin@1.4.0 --save-dev
 ``
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './app/src/js/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app/dist'),
-        clean: true
-    },
-    module: {
-        rules: [
-            { test: /\.css$/, use: ['style-loader','css-loader']  }
-        ]
-    },
-    plugins: [ new HtmlWebpackPlugin({
-        template: './app/src/app.html',
-        filename: 'app.html',
-        hash: true
-    })]
-
-}
+  entry: "./app/src/js/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "app/dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./app/src/app.html",
+      filename: "app.html",
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.css'
+    }),
+  ],
+};
